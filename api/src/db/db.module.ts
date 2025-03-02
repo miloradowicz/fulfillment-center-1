ca77f1a8243a04bda235dcc40a2da1c1b5450a8c
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-03-02 22:32:45 +0600

    JE2-32 schemas registration moved to db module,  seeder configuration updated to work with both relative and absolute paths

---
+++ api/src/db/db.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import config from '../config'
+import { Arrival, ArrivalSchema } from '../schemas/arrival.schema'
+import { Client, ClientSchema } from '../schemas/client.schema'
+import { Product, ProductSchema } from '../schemas/product.schema'
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(new URL(config.mongo.db, config.mongo.host).href),
+    MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]),
+    MongooseModule.forFeature([{ name: Arrival.name, schema: ArrivalSchema }]),
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
+  exports: [MongooseModule],
+})
+export class DbModule {}
