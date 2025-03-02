9a79fdfc155a67aa08f492bc71dd7ca0d78976ea
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-03-02 22:01:32 +0600

    JE2-12 Added products service and controller

---
+++ api/src/products/products.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common'
+import { CreateProductDto } from '../dto/create-product.dto'
+import { ProductsService } from './products.service'
+import { UpdateProductDto } from '../dto/update-product.dto'
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post() async createProduct(@Body() productDto: CreateProductDto) {
+    return await this.productsService.create(productDto)
+  }
+
+  @Get() async getAllProducts(@Query('client') clientId?: string) {
+    if (clientId) {
+      return await this.productsService.getAllByClient(clientId)
+    } else {
+      return await this.productsService.getAll()
+    }
+  }
+
+  @Get(':id') async getProduct(@Param('id') id: string) {
+    return await this.productsService.getById(id)
+  }
+
+  @Delete(':id') async deleteProduct(@Param('id') id: string) {
+    return await this.productsService.delete(id)
+  }
+
+  @Put(':id') async updateProduct(@Param('id') id: string, @Body() productDto: UpdateProductDto) {
+    return await this.productsService.update(id, productDto)
+  }
+}
---
+++ api/src/products/products.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { CreateProductDto } from '../dto/create-product.dto'
+import { UpdateProductDto } from '../dto/update-product.dto'
+import { Product, ProductDocument } from '../schemas/product.schema'
+
+@Injectable()
+export class ProductsService {
+  constructor(@InjectModel(Product.name) private readonly productModel: Model<ProductDocument>) {}
+
+  async getById(id: string) {
+    const product = await this.productModel.findById(id)
+
+    if (!product) {
+      throw new NotFoundException('Товар не найден')
+    }
+
+    return product
+  }
+
+  async getAllByClient(clientId: string) {
+    return (await this.productModel.find({ client: clientId })).reverse()
+  }
+
+  async getAll() {
+    return (await this.productModel.find()).reverse()
+  }
+
+  async create(productDto: CreateProductDto) {
+    const product = await this.productModel.create(productDto)
+
+    return product
+  }
+
+  async delete(id: string) {
+    const product = await this.productModel.findByIdAndDelete(id)
+
+    if (!product) {
+      throw new NotFoundException('Товар не найден')
+    }
+
+    return { message: 'Товар успешно удален' }
+  }
+
+  async update(id: string, productDto: UpdateProductDto) {
+    const product = await this.productModel.findByIdAndUpdate(id, productDto)
+
+    if (!product) {
+      throw new NotFoundException('Товар не найден')
+    }
+
+    return product
+  }
+}
