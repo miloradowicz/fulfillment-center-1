7074c32dff2554d206dc1b5bbfa43a9cf97d72a7
Natalya Botsman <botsmannatashaa@gmail.com>
2025-03-02 23:02:21 +0600

    JE2-15 Imported arrivals service & controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { Client, ClientSchema } from './schemas/client.schema'
 import { ClientsController } from './clients/clients.controller'
 import { Arrival, ArrivalSchema } from './schemas/arrival.schema'
+import { ArrivalsController } from './arrivals/arrivals.controller';
+import { ArrivalsService } from './arrivals/arrivals.service';
 
 @Module({
   imports: [
@@ -10,7 +12,7 @@ import { Arrival, ArrivalSchema } from './schemas/arrival.schema'
     MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]),
     MongooseModule.forFeature([{ name: Arrival.name, schema: ArrivalSchema }]),
   ],
-  controllers: [ClientsController],
-  providers: [],
+  controllers: [ClientsController, ArrivalsController],
+  providers: [ArrivalsService],
 })
 export class AppModule {}
