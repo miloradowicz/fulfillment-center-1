ce71ee6aa7a0a16a1e7e7552931519259e90445f
Natalya Botsman <botsmannatashaa@gmail.com>
2025-03-02 22:51:15 +0600

    JE2-15 Created arrivals controller, used all methods from arrivals service

---
+++ api/src/arrivals/arrivals.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { CreateArrivalDto } from '../dto/create-arrival.dto'
+import { ArrivalsService } from './arrivals.service'
+import { UpdateArrivalDto } from '../dto/update-arrival.dto'
+
+@Controller('arrivals')
+export class ArrivalsController {
+  constructor(private arrivalsService: ArrivalsService) {}
+
+  @Get()
+  async getAllArrivals() {
+    return this.arrivalsService.getAll()
+  }
+
+  @Get(':id')
+  async getOneArrival(@Param('id') id: string) {
+    return this.arrivalsService.getOne(id)
+  }
+
+  @Post()
+  async createArrival(@Body() arrivalDto: CreateArrivalDto) {
+    return this.arrivalsService.create(arrivalDto)
+  }
+
+  @Put(':id')
+  async updateArrival(@Param('id') id: string, @Body() arrivalDto: UpdateArrivalDto) {
+    return await this.arrivalsService.update(id, arrivalDto)
+  }
+
+  @Delete(':id')
+  async deleteArrival(@Param('id') id: string) {
+    return this.arrivalsService.delete(id)
+  }
+}
