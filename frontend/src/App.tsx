72be70494ff9ae128dbe0896c9e61fb80263c269
Natalya Botsman <botsmannatashaa@gmail.com>
2025-03-04 23:44:42 +0600

    JE2-19 Added route for arrival form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import ClientForm from './features/clients/components/ClientForm.tsx'
 import { Container } from '@mui/material'
-
+import ArrivalForm from './features/arrivals/components/ArrivalForm.tsx'
 
 const App = () => {
   return <>
     <Container>
       <Routes>
+        <Route path='add-new-arrival' element={<ArrivalForm/>}/>
         <Route path='add-new-client' element={<ClientForm />} />
       </Routes>
     </Container>
