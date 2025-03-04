d814a4496f207ac0107cdb6f84941b993f3e3df8
Ashymov Aziret <aziretashym@gmail.com>
2025-03-04 18:12:16 +0600

    JE2-18. [FE] Created form creating new products.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import ClientForm from './features/clients/components/ClientForm.tsx'
-import { Container } from '@mui/material'
+import { Container, Typography } from '@mui/material'
+import ProductForm from './features/products/components/ProductForm.tsx'
 
 
 const App = () => {
   return <>
     <Container>
       <Routes>
-        <Route path='add-new-client' element={<ClientForm />} />
+        <Route path='/' element={<></>} />
+        <Route path='/add-new-client' element={<ClientForm />} />
+        <Route path='/add-new-product' element={<ProductForm />} />
+        <Route path="/*" element={<Typography variant={'h3'} textAlign="center">Not Found</Typography>}/>
       </Routes>
     </Container>
   </>
