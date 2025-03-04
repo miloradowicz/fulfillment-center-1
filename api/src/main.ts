cd786c686d8440f1f3deed80d2a183ba73d9668e
Ashymov Aziret <aziretashym@gmail.com>
2025-03-04 18:15:27 +0600

    JE2-18. [BE] Added "document" property to “create-product.dto.ts”. Also added a limitation for uploaded files.

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,11 +6,14 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationError, ValidationPipe } from '@nestjs/common'
 import { DtoValidationError, DtoValidationErrorFilter } from './exception-filters/dto-validation-error.filter'
 import { CastErrorFilter } from './exception-filters/mongo-cast-error.filter'
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors()
   app.useStaticAssets(join(config.rootPath, config.publicPath))
+  app.use(express.json({ limit: '10mb' }))
+  app.use(express.urlencoded({ limit: '10mb', extended: true }))
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[]) => {
