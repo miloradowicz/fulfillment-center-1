1332facee32512dc71628498bd9a73863af28128
Ashymov Aziret <aziretashym@gmail.com>
2025-03-08 17:58:06 +0600

    JE2-71 [FE] Implemented refactoring for ClientForm.

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,5 @@
 export const emailRegex = /^(\w+[-.]?\w+)@(\w+)([.-]?\w+)?(\.[a-zA-Z]{2,3})$/
+export const phoneNumberRegex = /^(\+?\d{1,3}[-.\s]?)?(\(?\d{1,4}\)?[-.\s]?)?(\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,4})$/
 
 export const roles = [
   { name: 'super-admin', title: 'Супер-пользователь' },
@@ -6,3 +7,13 @@ export const roles = [
   { name: 'manager', title: 'Менеджер' },
   { name: 'stock-worker', title: 'Складской работник' },
 ]
+
+export const initialClientState = {
+  name: '',
+  phone_number: '',
+  email: '',
+  inn: '',
+  address: '',
+  banking_data: '',
+  ogrn: '',
+}
