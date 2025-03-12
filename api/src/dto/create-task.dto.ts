6fbb2260c38ea32957708318ffb6419a5dc46927
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-03-12 19:55:58 +0600

    [JE2-86] Create task.schema.ts and create DTO and update.dto

---
+++ api/src/dto/create-task.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsArray,
+  IsEnum,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator'
+import { Type } from 'class-transformer'
+
+class LogDto {
+  @IsNotEmpty({ message: 'Поле пользователь не должно быть пустым.' })
+  @IsMongoId({ message: 'Некорректный формат ID' })
+  user: string
+
+  @IsNotEmpty({ message: 'Поле описание изменения не должно быть пустым.' })
+  change: string
+
+  @IsNotEmpty({ message: 'Поле дата изменения не должно быть пустым.' })
+  date: Date
+}
+
+export class CreateTaskDto {
+  @IsNotEmpty({ message: 'Поле исполнитель обязательно для заполнения' })
+  user: string
+
+  @IsNotEmpty({ message: 'Поле заголовок задачи обязательно для заполнения' }) title: string
+
+  @IsOptional()
+  description?: string
+
+  @IsEnum(['к выполнению', 'в работе', 'готово'], {
+    message: 'Статус должен быть одним из: "к выполнению", "в работе", "готово"',
+  })
+  status: 'к выполнению' | 'в работе' | 'готово'
+
+  @IsOptional()
+  @IsArray({ message: 'Список логов должен быть массивом.' })
+  @ValidateNested({ each: true })
+  @Type(() => LogDto)
+  logs?: LogDto[]
+
+}
