ed6d444fee1a558852e7dfed1e8c8a8623b52952
Natalya Botsman <botsmannatashaa@gmail.com>
2025-03-13 23:19:27 +0600

    [JE2-81] Created Stock schema

---
+++ api/src/schemas/stock.schema.ts
@@ -0,0 +1,47 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import mongoose from 'mongoose'
+
+export type StockDocument = Stock & Document
+
+@Schema({ timestamps: true })
+export class Stock {
+  @Prop({ required: true })
+  name: string
+
+  @Prop({ required: true })
+  address: string
+
+  @Prop({
+    type: [
+      {
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        description: { type: String },
+        amount: { type: Number, required: true },
+      },
+    ],
+    default: [],
+  })
+  products: {
+    product: mongoose.Schema.Types.ObjectId
+    description: string
+    amount: number
+  }[]
+
+  @Prop({
+    type: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        change: { type: String, required: true },
+        date: { type: Date, required: true, default: Date.now },
+      },
+    ],
+    default: [],
+  })
+  logs: {
+    user: mongoose.Schema.Types.ObjectId
+    change: string
+    date: Date
+  }[]
+}
+
+export const StockSchema = SchemaFactory.createForClass(Stock)
