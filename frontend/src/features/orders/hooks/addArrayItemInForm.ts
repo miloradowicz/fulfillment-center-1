e2aa11726fcc28a93f954a2829ccca5fc7e54aa7
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-03-16 18:02:22 +0600

    [JE2-96] Fixed typing error in hook order

--- frontend/src/features/orders/hooks/addArrayItemInForm.ts
+++ frontend/src/features/orders/hooks/addArrayItemInForm.ts
@@ -1,19 +1,18 @@
+import { Defect, DefectForOrderForm, Product, ProductForOrderForm, ProductOrder } from '../../../types'
 import React from 'react'
-import { Product } from '../../../types'
 
-export const addArrayItemInForm = (
-  // TODO fix any https://botsmannatashaa.atlassian.net/browse/JE2-96
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  newField: any,
-  // TODO fix any https://botsmannatashaa.atlassian.net/browse/JE2-96
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  setForm: React.Dispatch<React.SetStateAction<any[]>>,
-  setArrayData: () => void,
-  clientProducts: Product[] | null,
-  amountField: string,
-  descriptionField: string,
-  productField: string,
-) => {
+export const addArrayItemInForm = <
+  T extends ProductOrder | Defect,
+  U extends ProductForOrderForm | DefectForOrderForm
+>(
+    newField: T,
+    setForm: React.Dispatch<React.SetStateAction<U[]>>,
+    setArrayData: () => void,
+    clientProducts: Product[] | null,
+    amountField: keyof T,
+    descriptionField: keyof T,
+    productField: keyof T,
+  ) => {
   if (clientProducts) {
     for (let i = 0; i < clientProducts.length; i++) {
       if (newField[productField] === clientProducts[i]._id) {
@@ -23,11 +22,10 @@ export const addArrayItemInForm = (
             product: clientProducts[i],
             [amountField]: Number(newField[amountField]),
             [descriptionField]: newField[descriptionField],
-          },
+          } as unknown as U,
         ])
       }
     }
     setArrayData()
   }
 }
-
