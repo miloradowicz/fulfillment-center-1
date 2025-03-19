592492f2fb69be11408f45f231664a2c906df927
Ashymov Aziret <aziretashym@gmail.com>
2025-03-19 20:57:44 +0600

    [JE2-101] Table “Клиенты” adaptation for small screens has been implemented.

--- frontend/src/features/clients/containers/ClientPage.tsx
+++ frontend/src/features/clients/containers/ClientPage.tsx
@@ -1,11 +1,14 @@
 import Modal from '../../../components/UI/Modal/Modal.tsx'
-import { Box, Button, CircularProgress, Typography } from '@mui/material'
+import { Box, Button, CircularProgress, Typography, useMediaQuery, useTheme } from '@mui/material'
 import { useClientPage } from '../hooks/useClientPage.ts'
 import ClientsDataList from '../components/ClientsDataList.tsx'
 import ClientForm from '../components/ClientForm.tsx'
 
 const ClientsPage = () => {
   const { open, handleOpen, handleClose, isLoading } = useClientPage()
+
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
     <>
@@ -18,17 +21,18 @@ const ClientsPage = () => {
       <Modal handleClose={handleClose} open={open}>
         <ClientForm onClose={handleClose} />
       </Modal>
-      <Box
-        display={'flex'}
-        className="text-center mb-5 mt-7 text-[20px] flex items-center justify-between"
-      >
-        <Typography className="flex-grow" variant="h5">Клиенты</Typography>
+      <Box className="max-w-[1000px] mx-auto mb-5 mt-7 w-full flex items-center justify-end">
+        <Typography variant={isSmallScreen ? 'h6' : 'h5'} className="flex-grow">
+          Клиенты
+        </Typography>
         <Button
           sx={{
-            'color': '#32363F',
-            'marginLeft': 'auto',
-            'border': '1px solid #32363F',
-            'transition': 'all 0.3s ease-in-out',
+            color: '#32363F',
+            marginLeft: 'auto',
+            border: '1px solid #32363F',
+            transition: 'all 0.3s ease-in-out',
+            padding: isSmallScreen ? '1px 3px' : '3px 6px',
+            fontSize: isSmallScreen ? '10px' : '12px',
             '&:hover': {
               color: '#ffffff',
               backgroundColor: '#32363F',
