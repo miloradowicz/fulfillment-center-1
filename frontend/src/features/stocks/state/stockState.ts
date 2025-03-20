ce9cadefaf216d15bdedc1583a29d26429908ee1
Natalya Botsman <botsmannatashaa@gmail.com>
2025-03-20 16:23:04 +0600

    [JE2-104] Updated arrival initial state

--- frontend/src/features/stocks/state/stockState.ts
+++ frontend/src/features/stocks/state/stockState.ts
@@ -1,18 +1,9 @@
 export const initialState = {
   name: '',
   address: '',
-  products: [],
-}
-
-export const initialProductState = {
-  product: '',
-  description: '',
-  amount: 0,
 }
 
 export const initialErrorState = {
   name: '',
   address: '',
-  product: '',
-  amount: '',
 }
