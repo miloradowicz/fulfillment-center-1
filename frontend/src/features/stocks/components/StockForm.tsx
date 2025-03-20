adcdd5eeec781125e62b7956a602519300c95c2e
Natalya Botsman <botsmannatashaa@gmail.com>
2025-03-20 16:22:54 +0600

    [JE2-104] Updated arrival hook and form

--- frontend/src/features/stocks/components/StockForm.tsx
+++ frontend/src/features/stocks/components/StockForm.tsx
@@ -1,38 +1,21 @@
 import React from 'react'
-import { ProductArrival, StockPopulate } from '../../../types'
+import { Stock } from '../../../types'
 import { useStockForm } from '../hooks/useStockForm.ts'
 import Grid from '@mui/material/Grid2'
 import { Button, CircularProgress, TextField, Typography } from '@mui/material'
-import ItemsList from '../../arrivals/components/ItemsList.tsx'
-import Autocomplete from '@mui/material/Autocomplete'
 import { getFieldError } from '../../../utils/getFieldError.ts'
 import { inputChangeHandler } from '../../../utils/inputChangeHandler.ts'
-import { getProductNameById } from '../../../utils/getProductName.ts'
 
 interface Props {
-  initialData?: StockPopulate | undefined
+  initialData?: Stock | undefined
   onSuccess?: () => void
 }
 
 const StockForm: React.FC<Props> = ({ initialData, onSuccess }) => {
-  const {
-    products,
-    isLoading,
-    form,
-    setForm,
-    newItem,
-    setNewItem,
-    errors,
-    productsForm,
-    productsModalOpen,
-    setProductsModalOpen,
-    openModal,
-    addItem,
-    deleteItem,
-    handleBlur,
-    error,
-    submitFormHandler,
-  } = useStockForm(initialData, onSuccess)
+  const {  isLoading, form, setForm, errors, handleBlur, error, submitFormHandler } = useStockForm(
+    initialData,
+    onSuccess,
+  )
 
   return (
     <form onSubmit={submitFormHandler}>
@@ -76,86 +59,6 @@ const StockForm: React.FC<Props> = ({ initialData, onSuccess }) => {
             fullWidth
           />
         </Grid>
-
-        <Grid>
-          <Typography fontWeight="bold">Товары</Typography>
-          <ItemsList
-            items={productsForm}
-            onDelete={i => deleteItem(i)}
-            getProductNameById={i => getProductNameById(products, i)}
-          />
-          <Button type="button" onClick={() => openModal()}>
-            + Добавить товары
-          </Button>
-        </Grid>
-
-        {productsModalOpen && (
-          <Grid>
-            <Typography sx={{ marginBottom: '15px' }}>Укажите товары</Typography>
-            <Autocomplete
-              fullWidth
-              size="small"
-              disablePortal
-              options={products ?? []}
-              onChange={(_, newValue) => {
-                if (newValue) {
-                  setNewItem(prev => ({ ...prev, product: newValue._id }))
-                }
-              }}
-              getOptionLabel={option => `${ option.title }. Артикул: ${ option.article }`}
-              isOptionEqualToValue={(option, value) => option._id === value._id}
-              renderInput={params => (
-                <TextField
-                  {...params}
-                  label="Товар"
-                  error={Boolean(errors.product || getFieldError('product', error))}
-                  helperText={errors.product || getFieldError('product', error)}
-                  onBlur={e => handleBlur('product', e.target.value)}
-                />
-              )}
-              sx={{ marginBottom: '15px' }}
-            />
-
-            <TextField
-              type="number"
-              fullWidth
-              size="small"
-              name="amount"
-              label="Количество товара"
-              value={newItem.amount || ''}
-              onChange={e => setNewItem(prev => ({ ...prev, amount: +e.target.value }))}
-              error={Boolean(errors.amount || getFieldError('amount', error))}
-              helperText={errors.amount || getFieldError('amount', error)}
-              onBlur={e => handleBlur('amount', e.target.value)}
-              sx={{ marginBottom: '15px' }}
-            />
-
-            <TextField
-              label="Описание товара"
-              fullWidth
-              size="small"
-              name="description"
-              value={(newItem as ProductArrival).description || ''}
-              onChange={e =>
-                setNewItem(prev => ({
-                  ...prev,
-                  description: e.target.value,
-                }))
-              }
-              sx={{ marginBottom: '15px' }}
-            />
-
-            <Grid container spacing={2}>
-              <Button type="button" variant="outlined" onClick={() => addItem()}>
-                Добавить
-              </Button>
-
-              <Button type="button" variant="outlined" onClick={() => setProductsModalOpen(false)}>
-                Закрыть
-              </Button>
-            </Grid>
-          </Grid>
-        )}
 
         <Grid>
           <Button fullWidth type="submit" variant="contained" sx={{ mt: 3, mb: 2 }} disabled={isLoading}>
--- frontend/src/features/stocks/hooks/useStockForm.ts
+++ frontend/src/features/stocks/hooks/useStockForm.ts
@@ -1,6 +1,6 @@
-import { initialErrorState, initialProductState, initialState } from '../state/stockState.ts'
+import { initialErrorState, initialState } from '../state/stockState.ts'
 import React, { useEffect, useState } from 'react'
-import { ProductStock, ProductWithPopulate, StockError, StockMutation, StockPopulate } from '../../../types'
+import { Stock, StockError, StockMutation } from '../../../types'
 import { useAppDispatch, useAppSelector } from '../../../app/hooks.ts'
 import { selectAllProducts } from '../../../store/slices/productSlice.ts'
 import { selectIsStockCreating, selectStockCreateError } from '../../../store/slices/stocksSlice.ts'
@@ -8,7 +8,7 @@ import { fetchProducts } from '../../../store/thunks/productThunk.ts'
 import { toast } from 'react-toastify'
 import { addStock, fetchStockById, fetchStocks, updateStock } from '../../../store/thunks/stocksThunk.ts'
 
-export const useStockForm = (initialData?: StockPopulate, onSuccess?: () => void) => {
+export const useStockForm = (initialData?: Stock, onSuccess?: () => void) => {
   const dispatch = useAppDispatch()
   const products = useAppSelector(selectAllProducts)
   const error = useAppSelector(selectStockCreateError)
@@ -18,48 +18,15 @@ export const useStockForm = (initialData?: StockPopulate, onSuccess?: () => void
     initialData
       ? {
         ...initialData,
-        products: [],
       }
       : { ...initialState },
   )
 
-  const [productsForm, setProductsForm] = useState<ProductStock[]>(
-    initialData?.products
-      ? initialData.products.map(p => ({
-        product: (p.product as ProductWithPopulate)._id,
-        description: p.description,
-        amount: p.amount,
-      }))
-      : [],
-  )
-
-  const [newItem, setNewItem] = useState<ProductStock>({ ...initialProductState })
   const [errors, setErrors] = useState<StockError>({ ...initialErrorState })
-  const [productsModalOpen, setProductsModalOpen] = useState(false)
 
   useEffect(() => {
     dispatch(fetchProducts())
   }, [dispatch])
-
-  const openModal = () => {
-    setNewItem({ ...initialProductState })
-    setProductsModalOpen(true)
-  }
-
-  const addItem = () => {
-    if (!newItem.product || newItem.amount <= 0) {
-      toast.warn('Заполните все обязательные поля.')
-      return
-    }
-
-    setProductsForm(prev => [...prev, newItem])
-    setProductsModalOpen(false)
-    setNewItem({ ...initialProductState })
-  }
-
-  const deleteItem = (index: number) => {
-    setProductsForm(prev => prev.filter((_, i) => i !== index))
-  }
 
   const handleBlur = (field: keyof StockError, value: string | number) => {
     type ErrorMessages = {
@@ -69,8 +36,6 @@ export const useStockForm = (initialData?: StockPopulate, onSuccess?: () => void
     const errorMessages: ErrorMessages = {
       name: !value ? 'Заполните название склада.' : '',
       address: !value ? 'Укажите адрес склада.' : '',
-      product: !value ? 'Выберите товар.' : '',
-      amount: Number(value) <= 0 ? 'Количество должно быть больше 0.' : '',
     }
 
     setErrors(prev => ({
@@ -87,29 +52,18 @@ export const useStockForm = (initialData?: StockPopulate, onSuccess?: () => void
       return
     }
 
-    const payload = {
-      ...form,
-      products: productsForm.map(p => ({
-        product: p.product,
-        description: p.description,
-        amount: p.amount,
-      })),
-    }
-
     try {
       if (initialData) {
-        await dispatch(updateStock({ stockId: initialData._id, stock: payload })).unwrap()
+        await dispatch(updateStock({ stockId: initialData._id, stock: form })).unwrap()
         onSuccess?.()
         toast.success('Склад успешно обновлен!')
         await dispatch(fetchStockById(initialData._id))
       } else {
-        await dispatch(addStock(payload)).unwrap()
+        await dispatch(addStock(form)).unwrap()
         await dispatch(fetchStocks())
         toast.success('Склад успешно создан!')
       }
-
       setForm({ ...initialState })
-      setProductsForm([])
     } catch (e) {
       console.error(e)
     }
@@ -120,15 +74,7 @@ export const useStockForm = (initialData?: StockPopulate, onSuccess?: () => void
     isLoading,
     form,
     setForm,
-    newItem,
-    setNewItem,
     errors,
-    productsForm,
-    productsModalOpen,
-    setProductsModalOpen,
-    openModal,
-    addItem,
-    deleteItem,
     handleBlur,
     error,
     submitFormHandler,
