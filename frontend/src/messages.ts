0e931e896fa777d9c13be1002f4a30b2a9d329f8
Natalya Botsman <botsmannatashaa@gmail.com>
2025-04-02 00:14:43 +0600

    [JE2-154] Moved types into enum, also created a list of error messages and passed it in forms and methods, created a separate interface for error fields

---
+++ frontend/src/messages.ts
@@ -0,0 +1,10 @@
+export enum ErrorMessagesList {
+  ProductErr = 'Выберите товар',
+  Amount = 'Количество товара должно быть больше 0',
+  DefectDescription = 'Заполните описание дефекта',
+  ClientErr = 'Выберите клиента',
+  ArrivalDate = 'Укажите дату прибытия',
+  StockErr = 'Выберите склад',
+  ArrivalPrice = 'Цена доставки должна быть больше 0.',
+  Default = '',
+}
