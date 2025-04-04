e793ed7bcac81118904a238b1703d2f3251eff79
Ashymov Aziret <aziretashym@gmail.com>
2025-04-05 04:32:43 +0600

    [JE2-145] Added requests to get all archived clients and for unarchive them, also added slices for that.

--- frontend/src/store/slices/clientSlice.ts
+++ frontend/src/store/slices/clientSlice.ts
@@ -1,14 +1,26 @@
 import { Client, GlobalError, ValidationError } from '../../types'
 import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../../app/store.ts'
-import { addClient, archiveClient, deleteClient, fetchClientById, fetchClients, updateClient } from '../thunks/clientThunk.ts'
+import {
+  addClient,
+  archiveClient,
+  deleteClient,
+  fetchArchivedClients,
+  fetchClientById,
+  fetchClients, unarchiveClient,
+  updateClient,
+} from '../thunks/clientThunk.ts'
 
 interface ClientState {
   client: Client | null;
+  archivedClient: Client | null;
   clients: Client[] | null;
+  archivedClients: Client[] | null;
   loadingFetch: boolean;
+  loadingArchived: boolean;
   loadingAdd: boolean;
   loadingArchive: boolean;
+  loadingUnarchive: boolean;
   loadingDelete: boolean;
   loadingUpdate: boolean;
   error: GlobalError | null;
@@ -18,10 +30,14 @@ interface ClientState {
 
 const initialState: ClientState = {
   client: null,
+  archivedClient: null,
   clients: null,
+  archivedClients: null,
   loadingFetch: false,
+  loadingArchived: false,
   loadingAdd: false,
   loadingArchive: false,
+  loadingUnarchive: false,
   loadingDelete: false,
   loadingUpdate: false,
   error: null,
@@ -30,8 +46,11 @@ const initialState: ClientState = {
 }
 
 export const selectClient = (state: RootState) => state.clients.client
+export const selectArchivedClient = (state: RootState) => state.clients.archivedClient
 export const selectAllClients = (state: RootState) => state.clients.clients
+export const selectAllArchivedClients = (state: RootState) => state.clients.archivedClients
 export const selectLoadingFetchClient = (state: RootState) => state.clients.loadingFetch
+export const selectLoadingArchivedClients = (state: RootState) => state.clients.loadingArchived
 export const selectLoadingAddClient = (state: RootState) => state.clients.loadingAdd
 export const selectLoadingArchiveClient = (state: RootState) => state.clients.loadingArchive
 export const selectLoadingDeleteClient = (state: RootState) => state.clients.loadingDelete
@@ -73,6 +92,16 @@ export const clientSlice = createSlice({
     builder.addCase(fetchClientById.rejected, state => {
       state.loadingFetch = false
     })
+    builder.addCase(fetchArchivedClients.pending, state => {
+      state.loadingArchived = true
+    })
+    builder.addCase(fetchArchivedClients.fulfilled, (state, action) => {
+      state.loadingArchived = false
+      state.archivedClients = action.payload
+    })
+    builder.addCase(fetchArchivedClients.rejected, state => {
+      state.loadingArchived = false
+    })
     builder.addCase(addClient.pending, state => {
       state.loadingAdd = true
       state.creationAndModificationError = null
@@ -96,6 +125,22 @@ export const clientSlice = createSlice({
     })
     builder.addCase(archiveClient.rejected, (state, { payload: error }) => {
       state.loadingArchive = false
+      state.error = error || null
+    })
+    builder.addCase(unarchiveClient.pending, state => {
+      state.loadingUnarchive = true
+      state.error = null
+    })
+    builder.addCase(unarchiveClient.fulfilled, (state, action) => {
+      state.loadingUnarchive = false
+      state.error = null
+
+      if (state.archivedClients) {
+        state.archivedClients = state.archivedClients.filter(client => client._id !== action.payload.id)
+      }
+    })
+    builder.addCase(unarchiveClient.rejected, (state, { payload: error }) => {
+      state.loadingUnarchive = false
       state.error = error || null
     })
     builder.addCase(deleteClient.pending, state => {
