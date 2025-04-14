234ed5c1bb6f612fee399804fd5b53396fa28a67
Natalya Botsman <botsmannatashaa@gmail.com>
2025-04-14 23:16:32 +0600

    [JE2-237] Set the theme for the app, changed color definitions. Deleted mui SideBar component

--- frontend/src/components/AppToolbar/AppToolbar.tsx
+++ frontend/src/components/AppToolbar/AppToolbar.tsx
@@ -16,63 +16,48 @@ const AppToolbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
   return (
-    <>
-      <header className="fixed top-0 left-0 w-full bg-slate-800 text-white z-50 border-b border-muted/20">
-        <div className="flex items-center justify-between h-16 px-4 sm:px-6 md:px-8">
-          <div className="flex items-center content-between gap-3">
-            {(!featureProtection || user) && (
-              <Sheet open={isOpen} onOpenChange={setIsOpen}>
-                <SheetTrigger asChild>
-                  <Button
-                    className="hover:bg-0 bg-0 text-white p-2 hover:text-slate-300 cursor-pointer transition-colors"
-                    type="button"
-                  >
-                    <Menu className="w-7 h-7" />
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="w-[250px] p-0 [&>button.absolute]:hidden">
-                  <VisuallyHidden>
-                    <DialogTitle>Навигация по сайту</DialogTitle>
-                    <DialogDescription>Сайдбар с навигацией по страницам приложения</DialogDescription>
-                  </VisuallyHidden>
-                  <SidebarContent onLinkClick={() => setIsOpen(false)} />
-                </SheetContent>
-              </Sheet>
-            )}
-
-            <NavLink to="/" className="bg-white rounded-md">
-              <img src="/logo.png" alt="logo" className="h-[40px] object-cover cursor-pointer" />
-            </NavLink>
-          </div>
-
-          <div className="flex items-center gap-3">
-            {user ? (
-              <UserMenu user={user} />
-            ) : (
-              <NavLink to="/login">
+    <header className="fixed top-0 left-0 w-full bg-primary text-primary-foreground z-50">
+      <div className="flex items-center justify-between h-16 px-4 sm:px-6 md:px-8">
+        <div className="flex items-center gap-3">
+          {(!featureProtection || user) && (
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
                 <Button
                   type="button"
-                  variant="outline"
-                  className="
-                  transition-colors
-                  cursor-pointer
-                  border-white
-                  text-slate-800
-                  hover:text-slate-800
-                  focus:text-slate-800
-                  hover:border-slate-300
-                  focus:border-slate-300
-                  hover:bg-slate-200
-                  focus:bg-slate-300"
+                  variant="ghost"
+                  className="transition-colors cursor-pointer"
                 >
-                  Войти
+                  <Menu className="w-7 h-7" />
                 </Button>
-              </NavLink>
-            )}
-          </div>
+              </SheetTrigger>
+              <SheetContent side="left" className="w-[250px] p-0 [&>button.absolute]:hidden">
+                <VisuallyHidden>
+                  <DialogTitle>Навигация по сайту</DialogTitle>
+                  <DialogDescription>Сайдбар с навигацией по страницам приложения</DialogDescription>
+                </VisuallyHidden>
+                <SidebarContent onLinkClick={() => setIsOpen(false)} />
+              </SheetContent>
+            </Sheet>
+          )}
+
+          <NavLink to="/" className="bg-card rounded-md">
+            <img src="/logo.png" alt="logo" className="h-[40px] object-cover cursor-pointer" />
+          </NavLink>
         </div>
-      </header>
-    </>
+
+        <div className="flex items-center gap-3">
+          {user ? (
+            <UserMenu user={user} />
+          ) : (
+            <NavLink to="/login">
+              <Button type="button" variant="secondary" className="cursor-pointer transition-colors font-bold">
+                Войти
+              </Button>
+            </NavLink>
+          )}
+        </div>
+      </div>
+    </header>
   )
 }
 
