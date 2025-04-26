3ec7098b2104b24b6d2301ef150aecc1881d2b6d
Ashymov Aziret <aziretashym@gmail.com>
2025-04-26 20:00:26 +0600

    [JE2-163] Hidden Sidebar list items depending on the role of the user.

--- frontend/src/components/SidebarContent/SidebarContent.tsx
+++ frontend/src/components/SidebarContent/SidebarContent.tsx
@@ -12,19 +12,7 @@ import {
   Settings,
 } from 'lucide-react'
 import React from 'react'
-
-const links = [
-  { to: '/admin', label: 'Админ-панель', icon: <Settings size={25} /> },
-  { to: '/clients', label: 'Клиенты', icon: <Users size={25} /> },
-  { to: '/products', label: 'Товары', icon: <Package size={25} /> },
-  { to: '/arrivals', label: 'Поставки', icon: <Truck size={25} /> },
-  { to: '/orders', label: 'Заказы', icon: <ClipboardList size={25} /> },
-  { to: '/tasks', label: 'Задачи', icon: <ListTodo size={25} /> },
-  { to: '/reports', label: 'Отчеты', icon: <FileText size={25} /> },
-  { to: '/stocks', label: 'Склады', icon: <Warehouse size={25} /> },
-  { to: '/counterparties', label: 'Контрагенты', icon: <BookUser size={25} /> },
-  { to: '/archives', label: 'Архив', icon: <ArchiveRestore size={25} /> },
-]
+import ProtectedElement from '@/components/ProtectedElement/ProtectedElement.tsx'
 
 interface Props {
   onLinkClick: () => void
@@ -33,20 +21,145 @@ interface Props {
 const SidebarContent: React.FC<Props> = ({ onLinkClick }) => {
   return (
     <nav className="flex flex-col gap-1 mt-4 px-2">
-      {links.map(({ to, label, icon }) => (
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager']}>
         <NavLink
-          key={to}
-          to={to}
+          to="/admin"
           onClick={onLinkClick}
           className={({ isActive }) =>
             `w-full flex items-center gap-2 p-2 rounded-md transition-colors
-             ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
           }
         >
-          {icon}
-          <span>{label}</span>
+          <Settings size={25} />
+          <span>Админ-панель</span>
         </NavLink>
-      ))}
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager']}>
+        <NavLink
+          to="/clients"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <Users size={25} />
+          <span>Клиенты</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager']}>
+        <NavLink
+          to="/products"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <Package size={25} />
+          <span>Товары</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager', 'stock-worker']}>
+        <NavLink
+          to="/arrivals"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <Truck size={25} />
+          <span>Поставки</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager', 'stock-worker']}>
+        <NavLink
+          to="/orders"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <ClipboardList size={25} />
+          <span>Заказы</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager', 'stock-worker']}>
+        <NavLink
+          to="/tasks"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <ListTodo size={25} />
+          <span>Задачи</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin']}>
+        <NavLink
+          to="/reports"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <FileText size={25} />
+          <span>Отчеты</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager']}>
+        <NavLink
+          to="/stocks"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <Warehouse size={25} />
+          <span>Склады</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin']}>
+        <NavLink
+          to="/counterparties"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <BookUser size={25} />
+          <span>Контрагенты</span>
+        </NavLink>
+      </ProtectedElement>
+
+      <ProtectedElement allowedRoles={['super-admin', 'admin', 'manager']}>
+        <NavLink
+          to="/archives"
+          onClick={onLinkClick}
+          className={({ isActive }) =>
+            `w-full flex items-center gap-2 p-2 rounded-md transition-colors
+            ${ isActive ? 'bg-ring/50 text-primary' : 'text-primary hover:bg-primary hover:text-card' }`
+          }
+        >
+          <ArchiveRestore size={25} />
+          <span>Архив</span>
+        </NavLink>
+      </ProtectedElement>
     </nav>
   )
 }
