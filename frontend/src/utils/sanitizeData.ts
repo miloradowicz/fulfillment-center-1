a4987a7b564c5e4c5a267c7b521f286925ea237f
Natalya Botsman <botsmannatashaa@gmail.com>
2025-04-29 20:58:46 +0600

    [JE2-229] Added corrected version of sanitizeData method

--- frontend/src/utils/sanitizeData.ts
+++ frontend/src/utils/sanitizeData.ts
@@ -13,12 +13,20 @@ export function sanitizeData<T>(data: T): T {
   } else if (Array.isArray(data)) {
     return data.map(item => sanitizeData(item)) as T
   } else if (typeof data === 'object' && data !== null) {
-    const sanitizedObj: { [key: string]: unknown } = {}
-    for (const key in data) {
-      const value = (data as Record<string, unknown>)[key]
-      sanitizedObj[key] = sanitizeData(value)
+    if (data instanceof FormData){
+      const sanitizedForm = new FormData
+      for (const [key, value] of data.entries()) {
+        sanitizedForm.append(key, sanitizeData(value))
+      }
+      return sanitizedForm as T
+    } else {
+      const sanitizedObj: { [key: string]: unknown } = {}
+      for (const key in data) {
+        const value = (data as Record<string, unknown>)[key]
+        sanitizedObj[key] = sanitizeData(value)
+      }
+      return sanitizedObj as T
     }
-    return sanitizedObj as T
   }
   return data
 }
