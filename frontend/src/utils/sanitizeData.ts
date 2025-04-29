d65098b9f789d1999e81b05784800852c9dd315c
Natalya Botsman <botsmannatashaa@gmail.com>
2025-04-29 21:10:35 +0600

    [JE2-229] Removed prev version of sanitizeData method, because files uploading wasn't working correctly

--- frontend/src/utils/sanitizeData.ts
+++ frontend/src/utils/sanitizeData.ts
@@ -12,21 +12,6 @@ export function sanitizeData<T>(data: T): T {
     return sanitizeString(data) as T
   } else if (Array.isArray(data)) {
     return data.map(item => sanitizeData(item)) as T
-  } else if (typeof data === 'object' && data !== null) {
-    if (data instanceof FormData){
-      const sanitizedForm = new FormData
-      for (const [key, value] of data.entries()) {
-        sanitizedForm.append(key, sanitizeData(value))
-      }
-      return sanitizedForm as T
-    } else {
-      const sanitizedObj: { [key: string]: unknown } = {}
-      for (const key in data) {
-        const value = (data as Record<string, unknown>)[key]
-        sanitizedObj[key] = sanitizeData(value)
-      }
-      return sanitizedObj as T
-    }
   }
   return data
 }
