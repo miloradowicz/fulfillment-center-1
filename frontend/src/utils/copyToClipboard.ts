e7ec3b0591906ff38ee1c98a4296a101b59a2982
Natalya Botsman <botsmannatashaa@gmail.com>
2025-04-29 21:36:56 +0600

    [JE2-229] Updated argument types

--- frontend/src/utils/copyToClipboard.ts
+++ frontend/src/utils/copyToClipboard.ts
@@ -1,4 +1,4 @@
-export const copyToClipboard = (text: string | null) => {
+export const copyToClipboard = (text: string | null | undefined) => {
   if (text) {
     const tempInput = document.createElement('input')
     document.body.appendChild(tempInput)
