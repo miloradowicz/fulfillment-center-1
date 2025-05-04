a1d8ee6a8fac78b406541acd09274c2b3e3948bf
Natalya Botsman <botsmannatashaa@gmail.com>
2025-05-04 14:13:32 +0600

    [JE2-218] Removed mui CssBaseLine from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
-import { CssBaseline } from '@mui/material'
 import { ToastContainer } from 'react-toastify'
 import { BrowserRouter } from 'react-router-dom'
 import { addCsrf, checkAuthentication } from './utils/axiosAPI.ts'
@@ -18,7 +17,6 @@ addCsrf()
 checkAuthentication(store)
   .then(() => createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-      <CssBaseline />
       <ToastContainer position={'top-center'} />
       <BrowserRouter>
         <App />
